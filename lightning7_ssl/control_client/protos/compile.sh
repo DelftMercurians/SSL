62b9e26595357f299f87b9f02ed4ff17800c0cdc
mablin7 <magyarb0201@gmail.com>
2023-03-25 13:07:30 +0100

    feat: add mypy stubs for proto files

--- lightning7_ssl/control_client/protos/compile.sh
+++ lightning7_ssl/control_client/protos/compile.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-protoc --python_out=../protobuf *.proto
+protoc --plugin=protoc-gen-mypy=$(poetry env info -p)/bin/protoc-gen-mypy --python_out=../protobuf/ --mypy_out=../protobuf/ *.proto
 
