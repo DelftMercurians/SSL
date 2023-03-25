e16fa123ddfe6cd0a510e8d61b2a41acd6c8b1b1
mablin7 <magyarb0201@gmail.com>
2023-03-25 20:49:50 +0100

    feat: only open ui in browser if not alredy open

--- lightning7_ssl/web/frontend/vite.config.ts
+++ lightning7_ssl/web/frontend/vite.config.ts
@@ -1,10 +1,36 @@
-import { defineConfig } from "vite";
+import { defineConfig, ViteDevServer } from "vite";
+import open from "open";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+
+/**
+ * A vite plugin that only open the UI in a browser if it's not already open.
+ */
+const openBrowserPlugin = (enabled: boolean = true) => ({
+  name: "open-browser",
+  configureServer(server: ViteDevServer) {
+    // return a post hook that is called after internal middlewares are
+    // installed
+    return () => {
+      if (!enabled) return;
+
+      // wait a bit then check if we have incoming websocket connections
+      setTimeout(() => {
+        if (server.ws.clients.size === 0) {
+          // if not, open the browser
+          server.config.logger.info("Opening browser...");
+          const adr = server.httpServer.address();
+          const url =
+            typeof adr === "string" ? adr : `http://localhost:${adr.port}`;
+          open(url);
+        }
+      }, 2000);
+    };
+  },
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    open: true,
     proxy:
       process.env.PROXY_PORT !== undefined
         ? {
@@ -20,5 +46,8 @@ export default defineConfig({
   build: {
     target: "es2020",
   },
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    openBrowserPlugin(process.env.OPEN_BROWSER !== undefined),
+  ],
 });
